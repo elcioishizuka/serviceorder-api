0a024da9815f1a516d76abe3912f70bbe70c64b3
Elcio <elcio.ishizuka@gmail.com>
2021-03-26 17:26:11 -0300

    First commit

---
+++ src/main/java/com/github/elcioishizuka/serviceorderapi/controller/ClientContoller.java
@@ -0,0 +1,9 @@
+package com.github.elcioishizuka.serviceorderapi.controller;
+
+public class ClientContoller {
+
+    public String list(){
+        return "Teste";
+    }
+
+}
---
+++ src/test/java/com/github/elcioishizuka/serviceorderapi/ServiceorderApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.elcioishizuka.serviceorderapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceorderApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
